ad67c4d28c35eb985e12dee9654cada8c557a657
Wisansiiz <18257719723@163.com>
2024-01-20 16:37:21 +0800

    feat: 用户登录

---
+++ controller/login.go
@@ -0,0 +1,22 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"online-voice-channel/models"
+)
+
+func Login(c *gin.Context) {
+	// 1. 从请求中把数据拿出来
+	var login models.Login
+	err := c.BindJSON(&login)
+	if err != nil {
+		return
+	}
+	err = models.LoginOn(&login)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		c.JSON(http.StatusOK, login)
+	}
+}

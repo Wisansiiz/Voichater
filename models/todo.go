071ec6ff07b2f152f82f377b7bfff865942b9fbf
Wisansiiz <18257719723@163.com>
2024-01-20 16:38:49 +0800

    feat: 数据校验与报错翻译

--- models/todo.go
+++ models/todo.go
@@ -2,23 +2,52 @@ package models
 
 import (
 	"errors"
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	zhtranslations "github.com/go-playground/validator/v10/translations/zh"
 	"gorm.io/gorm"
 	"online-voice-channel/dao"
+	"strings"
 )
 
 // Todo Model
 type Todo struct {
 	ID      uint           `json:"id"`
-	Title   string         `json:"title"`
-	Status  bool           `json:"status"`
+	Title   string         `json:"title" validate:"required"`
+	Status  bool           `json:"status" validate:"-"`
 	Deleted gorm.DeletedAt `json:"deleted"`
+}
+
+func Init() (v *validator.Validate, t ut.Translator) {
+	// 中文翻译器
+	zh_ch := zh.New()
+	uni := ut.New(zh_ch)
+	trans, _ := uni.GetTranslator("zh")
+	// 实例化验证对象
+	validate := validator.New()
+	// 验证器注册翻译器
+	_ = zhtranslations.RegisterDefaultTranslations(validate, trans)
+	return validate, trans
 }
 
 /*
 	Todo这个Model的增删改查操作都放在这里
 */
-// CreateATodo 创建todo
+// CreateTodo 创建todo
 func CreateTodo(todo *Todo) (err error) {
+	//err = dao.DB.Create(&todo).Error
+	//return
+	validate, trans := Init()
+	errs := validate.Struct(todo)
+	if errs != nil {
+		var sliceErrs []string
+		for _, err := range errs.(validator.ValidationErrors) {
+			//翻译错误信息
+			sliceErrs = append(sliceErrs, err.Translate(trans))
+		}
+		return errors.New(strings.Join(sliceErrs, ","))
+	}
 	err = dao.DB.Create(&todo).Error
 	return
 }

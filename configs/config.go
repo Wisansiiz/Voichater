c0798a076ab30bd97e691ba3f715de17542c728f
Wisansiiz <18257719723@163.com>
2024-01-22 18:35:26 +0800

    feat: 读取配置文件和环境变量

---
+++ configs/config.go
@@ -0,0 +1,87 @@
+package configs
+
+import (
+	"github.com/spf13/viper"
+	"os"
+	"regexp"
+)
+
+var Conf *AppConfig
+
+// AppConfig 应用程序配置
+type AppConfig struct {
+	Release       bool           `yaml:"release"`
+	Port          int            `yaml:"port"`
+	MySql         *MySqlConfig   `yaml:"mysql"`
+	Redis         *RedisConfig   `yaml:"redis"`
+	EncryptSecret *EncryptSecret `yaml:"encryptSecret"`
+}
+
+// MySqlConfig 数据库配置
+type MySqlConfig struct {
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	DB       string `yaml:"db"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+}
+
+// RedisConfig 数据库配置
+type RedisConfig struct {
+	RedisHost     string `yaml:"redisHost"`
+	RedisPort     string `yaml:"redisPort"`
+	RedisUsername string `yaml:"redisUsername"`
+	RedisPassword string `yaml:"redisPwd"`
+	RedisDbName   int    `yaml:"redisDbName"`
+	RedisNetwork  string `yaml:"redisNetwork"`
+}
+
+// EncryptSecret 加密的东西
+type EncryptSecret struct {
+	JwtSecret   string `yaml:"jwtSecret"`
+	EmailSecret string `yaml:"emailSecret"`
+	PhoneSecret string `yaml:"phoneSecret"`
+	MoneySecret string `yaml:"moneySecret"`
+}
+
+func replaceEnvVars(input string) string {
+	re := regexp.MustCompile("\\$\\{([^}]+)}")
+	return re.ReplaceAllStringFunc(input, func(match string) string {
+		envVarName := match[2 : len(match)-1]
+		return os.Getenv(envVarName)
+	})
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	vp := viper.New()
+	vp.SetConfigName("config")
+	vp.SetConfigType("yaml")
+	vp.AddConfigPath(workDir + "/configs/locales")
+	vp.AddConfigPath(workDir)
+	vp.AutomaticEnv()
+	err := vp.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	re := regexp.MustCompile("\\$\\{([^}]+)}")
+	for k, v := range vp.AllSettings() {
+		if mp, ok := v.(map[string]any); ok {
+			for k2, v2 := range mp {
+				if s, ok2 := v2.(string); ok2 {
+					if re.MatchString(s) {
+						err = vp.BindEnv(k2, v2.(string)[2:len(s)-1])
+						vp.Set(k+"."+k2, vp.Get(k2))
+					}
+				}
+			}
+		}
+	}
+	if err != nil {
+		panic(err)
+	}
+	err = vp.Unmarshal(&Conf)
+	if err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,33 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"online-voice-channel/configs"
 	"online-voice-channel/dao"
 	"online-voice-channel/models"
 	"online-voice-channel/routers"
-	"online-voice-channel/setting"
-
-	"fmt"
-	"os"
 )
 
-const defaultConfFile = "./conf/config.yaml"
-
 func main() {
-	confFile, err := os.ReadFile(defaultConfFile)
-	if err != nil {
-		panic(err)
-	}
 	// 加载配置文件
-	if err := setting.Init(confFile); err != nil {
-		fmt.Printf("load config from file failed, err:%v\n", err)
-		return
-	}
-	// 创建数据库
+	configs.InitConfig()
 	// 连接数据库
-	err = dao.InitMySQL(setting.Conf.DatabaseConfig)
+	err := dao.InitMySQL(configs.Conf.MySql)
 	if err != nil {
 		fmt.Printf("init mysql failed, err:%v\n", err)
-		return
+		panic(err)
 	}
 	defer dao.Close(dao.DB) // 程序退出关闭数据库连接
 	// 模型绑定
@@ -37,7 +25,8 @@ func main() {
 	}
 	// 注册路由
 	r := routers.SetupRouter()
-	if err := r.Run(fmt.Sprintf(":%d", setting.Conf.Port)); err != nil {
+	if err := r.Run(fmt.Sprintf(":%d", configs.Conf.Port)); err != nil {
 		fmt.Printf("server startup failed, err:%v\n", err)
+		return
 	}
 }

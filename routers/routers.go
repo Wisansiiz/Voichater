f93360d7d4b4b7d14369dcde011f6bdf31330229
Wisansiiz <18257719723@163.com>
2024-01-30 16:06:23 +0800

    feat: 添加实时通信的路由

--- routers/routers.go
+++ routers/routers.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"online-voice-channel/api"
 	"online-voice-channel/configs"
-	"online-voice-channel/controller"
 	"online-voice-channel/interceptor"
 )
 
@@ -13,37 +14,31 @@ func SetupRouter() *gin.Engine {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.Default()
-	// 告诉gin框架模板文件引用的静态文件去哪里找
-	//r.Static("/static", "static")
-	// 告诉gin框架去哪里找模板文件
-	//r.LoadHTMLGlob("templates/*")
-	//r.GET("/", controller.IndexHandler)
-	r.GET("/home", interceptor.ConfInterceptor(), homeHandler)
+	// 静态文件去
+	r.StaticFS("/static", http.Dir("./static"))
 
 	// v1
 	v1Group := r.Group("v1")
 	{
-		// 待办事项
-		// 添加
-		v1Group.POST("/todo", controller.CreateTodo)
-		// 查看所有的待办事项
-		v1Group.GET("/todo", controller.GetTodoList)
-		// 修改某一个待办事项
-		v1Group.PUT("/todo/:id", controller.UpdateATodo)
-		// 删除某一个待办事项
-		v1Group.DELETE("/todo/:id", controller.DeleteATodo)
+		authed := v1Group.Group("/") // 需要登陆保护
+		authed.Use(interceptor.ConfInterceptor())
+		{
+			// 添加待办事项
+			authed.POST("/todo", api.CreateTodo)
+			// 查看所有的待办事项
+			authed.GET("/todo", api.GetTodoList)
+			// 修改某一个待办事项
+			authed.PUT("/todo/:id", api.UpdateATodo)
+			// 删除某一个待办事项
+			authed.DELETE("/todo/:id", api.DeleteATodo)
+		}
 	}
 	loginGroup := r.Group("api")
 	{
-		loginGroup.POST("/login", controller.Login)
+		loginGroup.POST("/login", api.Login)
+		r.Use(cors.Default())
+		loginGroup.GET("/ws", api.Ws)
+		loginGroup.GET("/history", api.FindMessage)
 	}
 	return r
-}
-func homeHandler(c *gin.Context) {
-	username := c.MustGet("username").(string)
-	c.JSON(http.StatusOK, gin.H{
-		"code": 2000,
-		"msg":  "success",
-		"data": gin.H{"username": username},
-	})
 }

306b762eaacb72581fe49c924e1595f0f51f9d53
Wisansiiz <18257719723@163.com>
2024-02-03 12:37:46 +0800

    feat: 登录所需的数据结构体

--- models/user.go
+++ models/user.go
@@ -10,14 +10,13 @@ type User struct {
 	UserID           uint            `json:"user_id" gorm:"primarykey"`
 	Username         string          `json:"username" validate:"required,min=3,max=20"`
 	Email            string          `json:"email" validate:"required,email"`
-	PasswordHash     string          `json:"-" validate:"required,min=6"`
+	PasswordHash     string          `json:"password" validate:"required,min=6"`
 	AvatarURL        string          `json:"avatar_url"`
 	RegistrationDate time.Time       `json:"registration_date"`
-	LastLoginDate    time.Time       `json:"last_login_date"`
+	LastLoginDate    *time.Time      `json:"last_login_date"`
 	DeletedAt        *gorm.DeletedAt `gorm:"index"`
 }
-type UserResponse struct {
+type UserLoginResponse struct {
 	Username     string `json:"username"`
-	Email        string `json:"email"`
-	PasswordHash string `json:"-"`
+	PasswordHash string `json:"password"`
 }

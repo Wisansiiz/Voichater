69d22d963edb56d7ee2e07d04e0ec19b209b31e8
Wisansiiz <18257719723@163.com>
2024-02-03 19:45:30 +0800

    feat: 添加表单获取

--- models/user.go
+++ models/user.go
@@ -8,15 +8,15 @@ import (
 // User 表示用户表的结构体
 type User struct {
 	UserID           uint            `json:"user_id" gorm:"primarykey"`
-	Username         string          `json:"username" validate:"required,min=3,max=20"`
-	Email            string          `json:"email" validate:"required,email"`
-	PasswordHash     string          `json:"password" validate:"required,min=6"`
-	AvatarURL        string          `json:"avatar_url"`
+	Username         string          `json:"username" form:"username" validate:"required,min=3,max=20"`
+	Email            string          `json:"email" form:"email" validate:"required,email"`
+	PasswordHash     string          `json:"password" form:"password" validate:"required,min=6"`
+	AvatarURL        string          `json:"avatar_url" form:"avatar_url"`
 	RegistrationDate time.Time       `json:"registration_date"`
 	LastLoginDate    *time.Time      `json:"last_login_date"`
 	DeletedAt        *gorm.DeletedAt `gorm:"index"`
 }
 type UserLoginResponse struct {
-	Username     string `json:"username"`
-	PasswordHash string `json:"password"`
+	Username     string `json:"username" form:"username"`
+	PasswordHash string `json:"password" form:"password"`
 }

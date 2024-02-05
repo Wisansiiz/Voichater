7d127e99c8b9957f3fe6be1c11ce979b8d5a873d
Wisansiiz <18257719723@163.com>
2024-02-06 01:00:34 +0800

    fix: 去除测试用例

--- main.go
+++ main.go
@@ -17,8 +17,7 @@ func main() {
 	dao.InitRedis(configs.Conf.Redis)
 	defer dao.Close(dao.DB) // 程序退出关闭数据库连接
 	// 模型绑定
-	err := dao.DB.AutoMigrate(&models.Todo{})
-	err = dao.DB.AutoMigrate(&models.User{})
+	err := dao.DB.AutoMigrate(&models.User{})
 	err = dao.DB.AutoMigrate(&models.Message{})
 	if err != nil {
 		panic(err)

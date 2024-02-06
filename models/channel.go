e4efd15c349ff351e8184ed60db2a82cf1b65b96
Wisansiiz <18257719723@163.com>
2024-02-06 19:42:05 +0800

    refactor: 添加primaryKey

--- models/channel.go
+++ models/channel.go
@@ -4,7 +4,7 @@ import "time"
 
 // Channel 表示频道表的结构体
 type Channel struct {
-	ChannelID    int64     `json:"channel_id"`
+	ChannelID    int64     `json:"channel_id" gorm:"primaryKey"`
 	ChannelName  string    `json:"channel_name" validate:"required,min=3,max=50"`
 	ServerID     int64     `json:"server_id" validate:"required"`
 	Type         string    `json:"type" validate:"required"`
--- models/server.go
+++ models/server.go
@@ -4,7 +4,7 @@ import "time"
 
 // Server 表示服务器表的结构体
 type Server struct {
-	ServerID      int64     `json:"server_id"`
+	ServerID      int64     `json:"server_id" gorm:"primaryKey"`
 	ServerName    string    `json:"server_name" validate:"required,min=3,max=30"`
 	CreatorUserID int64     `json:"creator_user_id" validate:"required"`
 	CreationDate  time.Time `json:"creation_date"`

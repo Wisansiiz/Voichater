0a141d48d39aae20360a9b69cfb666b004bf420c
Wisansiiz <18257719723@163.com>
2024-02-09 13:20:33 +0800

    refactor: 提取跨域请求方法

--- routers/routers.go
+++ routers/routers.go
@@ -4,37 +4,16 @@ import (
 	"Voichatter/api"
 	"Voichatter/configs"
 	"Voichatter/interceptor"
+	"Voichatter/middleware"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
-
-func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin")
-		if origin != "" {
-			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-			c.Header("Access-Control-Allow-Credentials", "true")
-		}
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-		}
-		c.Next()
-	}
-}
 
 func SetupRouter() *gin.Engine {
 	if configs.Conf.Release {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.Default()
-	r.Use(Cors())
-	// 静态文件去
-	r.StaticFS("/static", http.Dir("./static"))
-
+	r.Use(middleware.Cors())
 	v := r.Group("api")
 	{
 		v.POST("/register", api.UserRegister)

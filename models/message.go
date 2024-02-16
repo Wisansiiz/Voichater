decac6e01f2ebaa5629c7f9ecb47cef6414cd7c2
Wisansiiz <18257719723@163.com>
2024-02-16 16:36:25 +0800

    refactor: 对齐变量值

--- models/message.go
+++ models/message.go
@@ -8,7 +8,7 @@ import (
 // Message 消息模型
 type Message struct {
 	MessageID    int64     `json:"message_id" gorm:"primaryKey"`
-	SenderUserID int64     `json:"sender_user_id" validate:"required"`
+	SenderUserID uint      `json:"sender_user_id" validate:"required"`
 	ChannelID    string    `json:"channel_id" validate:"required"`
 	Content      string    `json:"content" validate:"required"`
 	Attachment   string    `json:"attachment"`

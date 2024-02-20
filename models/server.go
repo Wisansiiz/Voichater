7528769a6087cc0a372632def30e16d2f6805bc6
Wisansiiz <18257719723@163.com>
2024-02-20 10:03:30 +0800

    feat: 添加服务器图片和类型

--- models/server.go
+++ models/server.go
@@ -7,6 +7,7 @@ type Server struct {
 	ServerID      int64     `json:"server_id" gorm:"primaryKey"`
 	ServerName    string    `json:"server_name" form:"server_name" validate:"required,min=2,max=20"`
 	CreatorUserID uint      `json:"creator_user_id" validate:"required"`
-	ServerTheme   string    `json:"server_theme" form:"server_theme" validate:"required"`
+	ServerType    string    `json:"server_type" form:"server_type"`
+	ServerImgUrl  string    `json:"server_img_url" form:"server_img_url"`
 	CreateDate    time.Time `json:"create_date"`
 }

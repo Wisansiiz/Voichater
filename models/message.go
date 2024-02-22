91905ed2766cb709c2693b17626263e1643ff7d6
Wisansiiz <18257719723@163.com>
2024-02-22 20:53:26 +0800

    feat: 添加删除时间

--- models/message.go
+++ models/message.go
@@ -7,11 +7,11 @@ import (
 
 // Message 消息模型
 type Message struct {
-	MessageID    int64     `json:"message_id" gorm:"primaryKey"`
-	SenderUserID uint      `json:"sender_user_id" validate:"required"`
-	ChannelID    string    `json:"channel_id" validate:"required"`
-	Content      string    `json:"content" validate:"required"`
-	Attachment   string    `json:"attachment"`
-	SendDate     time.Time `json:"send_date"`
-	gorm.Model
+	MessageID    int64          `json:"message_id" gorm:"primaryKey"`
+	SenderUserID uint           `json:"sender_user_id" validate:"required"`
+	ChannelID    string         `json:"channel_id" validate:"required"`
+	Content      string         `json:"content" validate:"required"`
+	Attachment   string         `json:"attachment"`
+	SendDate     time.Time      `json:"send_date"`
+	DeletedAt    gorm.DeletedAt `json:"deleted_at"`
 }

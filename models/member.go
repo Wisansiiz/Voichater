fe7edd31fbc4829e633fe3a3f9bc636215906fb4
Wisansiiz <18257719723@163.com>
2024-02-23 19:50:44 +0800

    feat: 添加用户对服务器和频道的权限控制

--- models/member.go
+++ models/member.go
@@ -4,9 +4,10 @@ import "time"
 
 // Member 表示成员表的结构体
 type Member struct {
-	MemberID    uint      `json:"member_id" gorm:"primaryKey"`
-	ServerID    uint      `json:"server_id" validate:"required"`
-	UserID      uint      `json:"user_id" validate:"required"`
-	JoinDate    time.Time `json:"join_date"`
-	Permissions string    `json:"permissions" validate:"required"`
+	MemberID     uint      `json:"member_id" gorm:"primaryKey"`
+	ServerID     uint      `json:"server_id" form:"server_id" validate:"required"`
+	UserID       uint      `json:"user_id" validate:"required"`
+	JoinDate     time.Time `json:"join_date"`
+	SPermissions string    `json:"s_permissions"`
+	CPermissions string    `json:"c_permissions"`
 }

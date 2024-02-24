67169459f0673815f413066db08334114ffb946d
Wisansiiz <18257719723@163.com>
2024-02-24 22:04:56 +0800

    feat: 添加软删除

--- models/member.go
+++ models/member.go
@@ -1,13 +1,16 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 // Member 表示成员表的结构体
 type Member struct {
-	MemberID     uint      `json:"member_id" gorm:"primaryKey"`
-	ServerID     uint      `json:"server_id" form:"server_id" validate:"required"`
-	UserID       uint      `json:"user_id" validate:"required"`
-	JoinDate     time.Time `json:"join_date"`
-	SPermissions string    `json:"s_permissions"`
-	CPermissions string    `json:"c_permissions"`
+	MemberID     uint            `json:"member_id" gorm:"primaryKey"`
+	ServerID     uint            `json:"server_id" form:"server_id" validate:"required"`
+	UserID       uint            `json:"user_id" validate:"required"`
+	JoinDate     time.Time       `json:"join_date"`
+	SPermissions string          `json:"s_permissions"`
+	DeletedAt    *gorm.DeletedAt `gorm:"index"`
 }
--- models/server.go
+++ models/server.go
@@ -1,13 +1,17 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 // Server 表示服务器表的结构体
 type Server struct {
-	ServerID      uint      `json:"server_id" gorm:"primaryKey"`
-	ServerName    string    `json:"server_name" form:"server_name" validate:"required,min=2,max=20"`
-	CreatorUserID uint      `json:"creator_user_id" validate:"required"`
-	ServerType    string    `json:"server_type" form:"server_type"`
-	ServerImgUrl  string    `json:"server_img_url" form:"server_img_url"`
-	CreateDate    time.Time `json:"create_date"`
+	ServerID      uint            `json:"server_id" gorm:"primaryKey"`
+	ServerName    string          `json:"server_name" form:"server_name" validate:"required,min=2,max=20"`
+	CreatorUserID uint            `json:"creator_user_id" validate:"required"`
+	ServerType    string          `json:"server_type" form:"server_type"`
+	ServerImgUrl  string          `json:"server_img_url" form:"server_img_url"`
+	CreateDate    time.Time       `json:"create_date"`
+	DeletedAt     *gorm.DeletedAt `gorm:"index"`
 }

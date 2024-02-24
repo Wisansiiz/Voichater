b915ce45374292a2348a589a6af85df188777866
Wisansiiz <18257719723@163.com>
2024-02-24 22:05:20 +0800

    feat: 服务器列表结构体

--- models/user.go
+++ models/user.go
@@ -20,3 +20,12 @@ type UserLoginResponse struct {
 	Username     string `json:"username" form:"username"`
 	PasswordHash string `json:"password" form:"password"`
 }
+
+type UserList4Server struct {
+	UserID        uint       `json:"user_id"`
+	Username      string     `json:"username"`
+	Email         string     `json:"email"`
+	AvatarURL     string     `json:"avatar_url"`
+	SPermissions  string     `json:"s_permissions"`
+	LastLoginDate *time.Time `json:"last_login_date"`
+}

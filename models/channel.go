e08993bbb93ded61f69558f68bac82765e1fbfe4
Wisansiiz <18257719723@163.com>
2024-02-25 19:40:10 +0800

    feat: 新增创建者字段

--- models/channel.go
+++ models/channel.go
@@ -4,9 +4,10 @@ import "time"
 
 // Channel 表示频道表的结构体
 type Channel struct {
-	ChannelID    int64     `json:"channel_id" gorm:"primaryKey"`
+	ChannelID    uint      `json:"channel_id" gorm:"primaryKey"`
 	ChannelName  string    `json:"channel_name" validate:"required,min=3,max=50"`
-	ServerID     int64     `json:"server_id" validate:"required"`
+	ServerID     uint      `json:"server_id" validate:"required"`
+	CreateUserId uint      `json:"create_user_id" validate:"required"`
 	Type         string    `json:"type" validate:"required"`
 	CreationDate time.Time `json:"creation_date"`
 }
